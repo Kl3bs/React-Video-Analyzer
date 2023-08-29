8d2b796d5f01ccb0b581b408c84aaeef53621c99
Kl3bs <kleberjunior58@gmail.com>
2023-08-29 16:15:07 -0300

    Firebase setup added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Storage, ref, uploadBytesResumable } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'video-analyzer';
+
+  private storage: Storage = inject(Storage);
+
+  uploadFile(input: HTMLInputElement) {
+    if (!input.files) return;
+
+    const files: FileList = input.files;
+
+    for (let i = 0; i < files.length; i++) {
+      const file = files.item(i);
+      if (file) {
+        const storageRef = ref(this.storage, file.name);
+        uploadBytesResumable(storageRef, file);
+      }
+    }
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'video-analyzer-26cb0',
+    appId: '1:98090469729:web:8dd59e8ec9af705fd9dd20',
+    storageBucket: 'video-analyzer-26cb0.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyC63hWM4dA3MUDg2uKUhgeFk8QHiQ-kg_8',
+    authDomain: 'video-analyzer-26cb0.firebaseapp.com',
+    messagingSenderId: '98090469729',
+    measurementId: 'G-882JET8QVG',
+  },
+};
